bc3d3ac69f1d7767fdc6db763549d8c194aa407e
Kate Vong <katiejnete@gmail.com>
2024-07-08 21:17:10 -0700

    complete shuffle the deck

--- src/Deck.jsx
+++ src/Deck.jsx
@@ -5,8 +5,9 @@ import "./Deck.css"
 
 const Deck = () => {
   const deckId = useRef();
-  const [deck, setDeck] = useState([]);
+  const [drawnCards, setDrawnCards] = useState([]);
   const [cardsRemaining, setCardsRemaining] = useState(52);
+  const [isShuffling, setIsShuffling] = useState(false);
 
   useEffect(() => {
     const fetchNewDeck = async () => {
@@ -32,22 +33,47 @@ const Deck = () => {
           `https://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=1`
         );
         const newCard = res.data.cards[0];
+        if (!newCard) throw new Error("No cards remaining!");
         newCard.rotation = `rotate(${Math.floor(Math.random()*361)}deg)`;
-        setDeck((prevDeck) => [...prevDeck, newCard]); 
+        setDrawnCards((prevDrawn) => [...prevDrawn, newCard]); 
       } catch (error) {
         console.error("Error drawing card:", error);
-        alert("Error: no cards remaining!");
+        alert("Error: No cards remaining!");
       }
     };
 
     if (cardsRemaining !== 52) addCard();
   }, [cardsRemaining]);
 
+  const shuffleDeck = () => {
+    if (isShuffling) return;
+    setIsShuffling(true);
+  }
+
+  useEffect(() => {
+    const shuffleCards = async () => {
+      try {
+        const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
+        deckId.current = res.data.deck_id;
+        setDrawnCards([]);
+        setCardsRemaining(52);
+      } catch (error) {
+        console.error("Error shuffling deck:", error);
+      }
+    };
+
+    if (isShuffling) {
+      shuffleCards();
+      setIsShuffling(false);
+    }
+  })
+
   return (
     <>
       {cardsRemaining >= 0 && <button onClick={drawCard}>Give me a card!</button>}
+      <button onClick={shuffleDeck}>Shuffle</button>
       <div>
-      {deck && deck.map(({ code, image, rotation }) => <Card key={code} image={image} rotation={rotation}/>)}
+      {!isShuffling && drawnCards && drawnCards.map(({ code, image, rotation }) => <Card key={code} image={image} rotation={rotation}/>)}
       </div>
     </>
   );
